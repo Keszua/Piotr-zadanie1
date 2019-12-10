16e204b7681a2f521cd81b51b1275995aed7f4f3
kmichalczyk <k.michalczyk@radwag.pl>
2019-12-10 13:39:12 +0100

    Updates

---
+++ precache-manifest.b1328006f36261cdbbd868221c8f54ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53f9ae814a25e8144a7391041e03a74",
+    "url": "/Piotr-zadanie1/index.html"
+  },
+  {
+    "revision": "2e9578d2a331d5dc64e0",
+    "url": "/Piotr-zadanie1/static/css/main.d69d5872.chunk.css"
+  },
+  {
+    "revision": "351a9d8467a6ca248c13",
+    "url": "/Piotr-zadanie1/static/js/2.590df3c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Piotr-zadanie1/static/js/2.590df3c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e9578d2a331d5dc64e0",
+    "url": "/Piotr-zadanie1/static/js/main.81c3cb4b.chunk.js"
+  },
+  {
+    "revision": "995bd97ed25f6e04a203",
+    "url": "/Piotr-zadanie1/static/js/runtime-main.1d589640.js"
+  }
+]);
